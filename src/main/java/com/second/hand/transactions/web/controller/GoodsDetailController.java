a1c769f380ef120e1a8552f043a504d1489d7bd3
liu jiachen <573324982@qq.com>
2019-06-11 15:12:07 +0800

    添加商品详情列表

---
+++ src/main/java/com/second/hand/transactions/web/controller/GoodsDetailController.java
@@ -0,0 +1,27 @@
+package com.second.hand.transactions.web.controller;
+
+import com.second.hand.transactions.service.GoodsService;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IDEA
+ * author:G.B.Monkey
+ * Date:2019/6/4 0004
+ * Time:20:32
+ */
+@RestController
+public class GoodsDetailController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @GetMapping("/goodsDetail")
+    public JSONObject goodsDetail(@RequestParam("goodsId") Integer goodsId){
+        JSONObject jsonObject = goodsService.goodsDetail(goodsId);
+        return jsonObject;
+    }
+}
